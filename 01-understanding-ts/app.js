396e46a21ee93af22df8c39bc8b845e2441cd6f0
Josh <zylo.codes@gmail.com>
2021-11-22 21:57:39 -0600

    Enums

--- 01-understanding-ts/app.js
+++ 01-understanding-ts/app.js
@@ -1,10 +1,12 @@
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READONLY"] = 1] = "READONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
 var person = {
     name: 'Josh',
     age: 33,
     hobbies: ['Sports', 'Cooking'],
-    role: [2, 'author'] // (number | string)[]
+    role: Role.ADMIN // (number | string)[]
 };
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
-}
--- 01-understanding-ts/app.ts
+++ 01-understanding-ts/app.ts
@@ -1,10 +1,9 @@
+enum Role { ADMIN, READONLY, AUTHOR }
+
 const person = {
   name: 'Josh',
   age: 33,
   hobbies: ['Sports', 'Cooking'], // string[]
-  role: [2, 'author'] // (number | string)[]
+  role: Role.ADMIN // (number | string)[]
 }
 
-for (const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase()) 
-}
